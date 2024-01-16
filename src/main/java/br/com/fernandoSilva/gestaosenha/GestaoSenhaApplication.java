1a97dce9ec9d62e2d574d2617941d47ccda3f247
Fernando Henrique da Silva <feroficialvolei@gmail.com>
2024-01-16 13:23:07 -0300

    primeiro commit ! construção do projeto base dá seguimento na aplicação

---
+++ src/main/java/br/com/fernandoSilva/gestaosenha/GestaoSenhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernandoSilva.gestaosenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoSenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoSenhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fernandoSilva/gestaosenha/GestaoSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernandoSilva.gestaosenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
